3b90c7a224932b229ceb8e932294ff2cc917902e
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-10-17 11:49:12 -0400

    qwdqwdq

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
-
-const thing1 = {
+const things = [
+{
     firstName: "Thing1",
     lastName: "Seus",
     graduated: true, 
     favoritecolor: "Red",
     Age: "16",
     Likes: ["bread", "pizza"],
-};
- const thing2 = {
+},
+{
     firstName: "Thing2",
     lastName: "Sooss",
     graduated: false, 
     favoritecolor: "Blue",
     Age: "20",
     Likes: ["icecream", "cheese"],
-};  
-const thing3 = {
+}, 
+{
     firstName: "Thing3",
     lastName: "Seuss",
     graduated: true, 
     favoritecolor: "Green",
     Age: "17",
     Likes: ["chocolate", "jellybeans"],
-};
- const thing4 = {
+},
+{
     firstName: "Thing4",
     lastName: "Seuz",
     graduated: false, 
     favoritecolor: "Purple",
     Age: "5",
     Likes: ["football", "dancing"],
-};  
+},
+]
 
-const array1 = [thing1, thing2, thing3, thing4];
+things.forEach(element => {
+    console.log(element.firstName);
+    console.log(element.lastName);
+    console.log(element.graduated);
+    console.log(element.favoritecolor);
+    console.log(element.Age);
+    console.log(element.Likes);
+});
+things.forEach((element) => element.Likes.forEach((element) => console.log(element.Likes)));
+
+const graduated = things.filter((things)=> things.graduated !== true);
+console.log(graduated);
+
+/* things.forEach((element)=>element.Likes.forEach((element) => console.log(Likes)));
+ */
+/* array1.forEach((thing)=>thing.Likes.forEach((thing) => console.log(Likes)));
+
+/* const array1 = [thing1, thing2, thing3, thing4];
 array1.forEach((name)=> console.log(name.firstName));
 const array2 = [thing1, thing2, thing3, thing4];
 array2.forEach((lastname)=> console.log(lastname.lastName));
@@ -45,14 +63,7 @@ array4.forEach((color)=> console.log(color.favoritecolor));
 const array5 = [thing1, thing2, thing3, thing4];
 array5.forEach((age)=> console.log(age.Age));
 
-/* const array6 = [thing1, thing2, thing3, thing4];
-const list = [thing1, thing2, thing3, thing4];
-array6.forEach((list) => list.forEach((like)=> console.log(like.Likes))); */
 const array6 = [thing1, thing2, thing3, thing4];
-array6.forEach((like)=> console.log(like.Likes));
-
-
-//filter array//
-const array8 = [thing1, thing2, thing3, thing4];
-const graduated = array8.filter((array8)=> array8.graduated !== true);
-console.log(graduated);
+const list = [thing1, thing2, thing3, thing4];
+array6.forEach((list) => list.forEach((like)=> console.log(like.Likes)));  
+*/
